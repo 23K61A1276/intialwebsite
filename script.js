8e53befc03b63120300c066e865be5f20b582090
23K61A1276 <152159292+23K61A1276@users.noreply.github.com>
2026-06-15 13:17:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-function showMessage(){
-    alert("Website is working!");
-}
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+        document.getElementById("message").innerHTML =
+            "Login Successful!";
+    } else {
+        document.getElementById("message").innerHTML =
+            "Invalid Username or Password";
+    }
+});
